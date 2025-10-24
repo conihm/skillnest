f9747ae977f705292750758561107f93db4fcbcd
Constanza Hurtado Meneses <constanza.hurtado.meneses@gmail.com>
2025-10-23 23:48:51 -0300

    core 7

---
+++ core7_onClick/script.js
@@ -0,0 +1,30 @@
+let btnIniciarSesion = document.getElementById("iniciar_sesion")
+btnIniciarSesion.addEventListener("click", function () {
+    let text = btnIniciarSesion.textContent;
+    if (text == "Iniciar Sesión") btnIniciarSesion.textContent = "Cerrar Sesión";
+    else btnIniciarSesion.textContent = "Iniciar Sesión";
+});
+
+
+
+let btnAgregarDef = document.getElementById("agregar_def")
+btnAgregarDef.addEventListener("click", function () {
+    //btnAgregarDef.style.visibility = "hidden";
+    btnAgregarDef.remove();
+});
+
+
+let btnMeGusta = document.querySelectorAll(".me_gusta");
+btnMeGusta.forEach(function (boton) {
+    let contador = 0;
+    boton.addEventListener('click', function () {
+        contador++;
+        boton.textContent = contador + " Me gusta";
+        const mascota = boton.closest(".card").querySelector("h2").textContent;
+        console.log(mascota);
+
+        alert(mascota + " was liked!");
+    });
+
+});
+
